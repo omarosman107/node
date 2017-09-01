6a07a4cd871c950c11740c5469b8fcf9fbd57bda
omarosman107 <omarosman107@gmail.com>
2017-08-31 21:17:49 -0400

    Update nodeapp.js

--- nodeapp.js
+++ nodeapp.js
@@ -17,14 +17,13 @@ const PORT = process.env.PORT || 8080;
 app.get('/', function(req, res) {
 
      var showname = req.query.url
-     fetch(req.query.url).then(function(res){ console.log(res.headers);return res.text()}).then(function(data){
+     fetch(req.query.url).then(function(res){   res.setHeader('Content-Type', res.headers['content-type']);return res.text()}).then(function(data){
       res.send(data);
 
      })
    console.log('User Is connecting')
         console.log(showname)
 
-   res.setHeader('Content-Type', 'application/json');
 res.setHeader('Cache-Control', 'public, max-age=246000');
     res.header('Access-Control-Allow-Origin', '*');
 
